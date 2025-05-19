603eb99a1400ebc3d38fc0694bb62b1171102fc5
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-19 22:14:18 +0300

    Added comments to the code

--- task1.py
+++ task1.py
@@ -6,9 +6,9 @@
 #  return TOTAL
 # END
 
-def add(lists):
-    total=0
-    for element in lists:
-        total+=element
-    return total
-print(add([1,2,3,4]))
+def add(lists): #Defined a function that takes a list as a parameter
+    total=0     #intialized a variable total to 0 to store the sum after each traversal
+    for element in lists: # traverse the list and store each number in a variable called element
+        total+=element    # sum up the element in the list to the total 
+    return total          # return the sum of the list
+print(add([1,2,3,4]))     
