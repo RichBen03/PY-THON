112fcb5917fb7c37c89af63c7f5fc7664493a555
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-27 14:57:22 +0300

    Using match-case statements for pattern recognition and decision making

--- Control Flows/match-case.py
+++ Control Flows/match-case.py
@@ -1,4 +1,6 @@
 def checkVowel(n):
+    if (len(n)> 1):
+        return "Please enter an alphabet between [a-z]"
     match n:
         case 'a': return "This is a vowel"
         case 'e': return "This is a vowel"
@@ -6,8 +8,10 @@ def checkVowel(n):
         case 'o': return "This is a vowel"
         case 'u': return "This is a vowel"
         case _: return "This is a consonant"
-        
+
 print(checkVowel('a'))
 print(checkVowel('z'))
 print(checkVowel('f'))
 print(checkVowel('i'))
+print(checkVowel('o'))
+print(checkVowel('po'))
