43e9499e18babffb413cbba71cc3f7845e3f30bd
Rich Benjamin <richmwendwa03@gmail.com>
2025-06-07 17:21:21 +0300

    Created a list and used a nested loop to print the alternate values of the lists

---
+++ Control Flows/nested-while.py
@@ -0,0 +1,16 @@
+list1 = [1,2,3]
+list2 = [4,5,6]
+
+# for i in list1:
+#     print(i, end=",")
+#     for j in list2:
+#         print(j,end=",")
+#     print()
+    
+i=0 
+while i < len(list1):
+    j=0
+    while j < len(list2):
+        print(list1[i],list2[j])
+        j+=1
+    i+=1
