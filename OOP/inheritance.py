e0de1f4708f2e814624482a2223a530c4ad603a5
Rich Benjamin <richmwendwa03@gmail.com>
2025-06-22 14:24:48 +0300

    Learned how to use different types of inheritance

---
+++ OOP/inheritance.py
@@ -0,0 +1,39 @@
+class Dog:
+    def __init__(self,name):
+        self.name= name
+
+    def display(self):
+        print(f"The dog's name is {self.name}")
+
+# Single inheritance which inherits from the class Dog
+class Labrador(Dog):
+    def sound(self):
+        print("Labrador barks")
+
+# Initiliazing multilevel inheritance where a child class inherits from a parent class which then inherits from a another class
+class GuideDog(Labrador):
+    def guide(self):
+        print(f"{self.name} Guides the way")
+class German:
+    def greet(self):
+        print('German Dog says Hello')
+class Germany(German,Dog):
+    def sound(self):
+        print("German Dog barks")
+
+lab = Labrador("Billy")
+lab.display()
+lab.sound()
+
+guider = GuideDog("Njoro")
+guider.display()
+guider.sound()
+guider.guide()
+
+germans = Germany("Maxy")
+germans.display()
+germans.greet()
+germans.sound()
+    
+
+        
