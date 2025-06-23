99847bc701fdf857d45601a2649892c6724eddad
Rich Benjamin <richmwendwa03@gmail.com>
2025-06-23 15:47:32 +0300

    Added the updating the quantity and the removing an item

--- OOP/exercise2.py
+++ OOP/exercise2.py
@@ -23,6 +23,7 @@ class ProductCatalog:
 
     def add_products(self, product:Product):
         self.products.append(product)
+        
 
     def total_invetory_value(self)-> float:
          return sum(product.total_value() for product in self.products) 
@@ -31,27 +32,65 @@ class ProductCatalog:
         print("The product catalog includes: ")
         for product in self.products:
             print("-", product)
-            
+
+    def remove_product(self, name:str)->bool:
+        for product in self.products:
+            if product.name == name:
+                self.products.remove(product)
+                return  True, f"{product.name} was removed"
+        return False, f"{name} was not found"
     
+    def update_quantity(self, name:str, new_quantity:int):
+        for product in self.products:
+            if product.name == name:
+                if new_quantity <0:
+                    print("Quantity cannot be zero")
+                product.quantity = new_quantity
+                return  True, f"{product.name} was found and updated"
+            
+        return False, f"{name} was not found"
+
+          
+                
+       
 if __name__ == "__main__":
     # Creating objects from the class Products
     soap = Product("Geisha", 200, 1)
     rice = Product("Pishori", 120, 3)
     soda = Product("Coke", 50, 2)
+    milk = Product("Brookside", 60, 8)
     
     # Create the object FROM product catalog
     catalog = ProductCatalog()
+
+    # Add products to catalog
     catalog.add_products(rice)
     catalog.add_products(soda)
     catalog.add_products(soap)
+    catalog.add_products(milk)
 
     # List the products in the catalog
-    catalog.list_products()
+    print("\nItems in the Catalog:")
+    print(catalog.list_products())
 
     # List the inventory value
     print("The total invetory value is: ", catalog.total_invetory_value())
 
+    # removing an item from the catalog
+    print("Removing an item")
+    removed1=catalog.remove_product("Geisha")
+    print(removed1)
 
+    print("\n")
+
+    print("Removing an item")
+    removed2= catalog.remove_product("Sunlight")
+    print(removed2)
+     
+    print("\n")
+
+    print(catalog.update_quantity("Brookside",2 ))
+   
 
     
         
