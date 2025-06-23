cf74e2efecc3790f0803596afd8fa15db0db244f
Rich Benjamin <richmwendwa03@gmail.com>
2025-06-23 14:19:01 +0300

    Modified the code to accept a catalog

--- OOP/exercise2.py
+++ OOP/exercise2.py
@@ -13,9 +13,45 @@ class Product:
     def total_value(self) -> float:
         return self.quantity * self.price
     
-soap = Product("Geisha",200,5)
-due_price = soap.total_value()
-print(f"The total cost for your {soap.name} products is KES: {due_price}")
+    def __str__(self):
+        return f"{self.name}: {self.quantity} each costing KES {self.price}, will all amount to {self.total_value()}"
+
+
+class ProductCatalog:
+    def __init__(self):
+        self.products = []
+
+    def add_products(self, product:Product):
+        self.products.append(product)
+
+    def total_invetory_value(self)-> float:
+         return sum(product.total_value() for product in self.products) 
+    
+    def list_products(self):
+        print("The product catalog includes: ")
+        for product in self.products:
+            print("-", product)
+            
+    
+if __name__ == "__main__":
+    # Creating objects from the class Products
+    soap = Product("Geisha", 200, 1)
+    rice = Product("Pishori", 120, 3)
+    soda = Product("Coke", 50, 2)
+    
+    # Create the object FROM product catalog
+    catalog = ProductCatalog()
+    catalog.add_products(rice)
+    catalog.add_products(soda)
+    catalog.add_products(soap)
+
+    # List the products in the catalog
+    catalog.list_products()
+
+    # List the inventory value
+    print("The total invetory value is: ", catalog.total_invetory_value())
+
+
 
     
         
